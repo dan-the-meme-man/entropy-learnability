softmax option fixed

main.py
@@ -30,8 +30,11 @@ def main():
     argparser = argparse.ArgumentParser()
     argparser.add_argument('--distribution', '-d', type=str, choices=distributions)
     argparser.add_argument('--vocab_size', '-v', type=int, choices=vocab_sizes)
-    argparser.add_argument('--softmax', '-s', type=bool, default=False)
+    argparser.add_argument('--softmax', '-s', action='store_true')
     args = argparser.parse_args()
+    
+    print(args.softmax)
+    exit()
 
     hparams = {
         'device': 'cuda' if torch.cuda.is_available() else 'cpu',