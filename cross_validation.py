191825bbe1747cc5e2dd109c032a97f3a91ae76c
Dan DeGenaro <daniel.degenaro@gmail.com>
2024-12-27 13:07:33 -0500

    parallelized

--- cross_validation.py
+++ cross_validation.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import random
 
 import torch
@@ -30,12 +31,6 @@ settings = {
     'return_token_type_ids': False,
 }
 
-gpt2_tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-gpt2_tokenizer.pad_token = gpt2_tokenizer.eos_token
-bert_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-roberta_tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
-xlmroberta_tokenizer = XLMRobertaTokenizer.from_pretrained('xlm-roberta-base')
-
 hparams = {
     'device': 'cuda' if torch.cuda.is_available() else 'cpu',
     'n_positions': settings['max_length'],
@@ -59,55 +54,64 @@ hparams = {
     'log_interval': 100
 }
 
-for tokenizer, save_name in zip(
-    [gpt2_tokenizer, bert_tokenizer, roberta_tokenizer, xlmroberta_tokenizer],
-    ['gpt2', 'bert', 'roberta', 'xlmroberta']
-):
-    for split in range(N_FOLDS):
-        
-        print('Split:', split)
-        print('Tokenizer:', save_name)
-        
-        test_data = []
-        with open(f'brown_data/brown_{split}.txt', 'r', encoding='utf-8') as f:
-            test_data = f.readlines()
-        
-        train_data = []
-        for i in range(N_FOLDS):
-            if i == split:
-                continue
-            train_data.extend(open(f'brown_data/brown_{i}.txt', 'r', encoding='utf-8').readlines())
+save_name = sys.argv[1]
 
-        def collate_fn(data):
-            return tokenizer(data, **settings)
-        
-        train_loader = DataLoader(
-            train_data,
-            batch_size=hparams['batch_size'],
-            shuffle=True,
-            collate_fn=collate_fn
-        )
+if save_name == 'gpt2':
+    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+    tokenizer.pad_token = tokenizer.eos_token
+elif save_name == 'bert':
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+elif save_name == 'roberta':
+    tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
+elif save_name == 'xlm-roberta':
+    tokenizer = XLMRobertaTokenizer.from_pretrained('xlm-roberta-base')
+    
+def collate_fn(data):
+    return tokenizer(data, **settings)
 
-        test_loader = DataLoader(
-            test_data,
-            batch_size=hparams['batch_size'],
-            shuffle=False,
-            collate_fn=collate_fn
-        )
-        
-        optimizer = get_optimizer(model, hparams)
-        
-        model = get_model(**hparams)
-        
-        train_and_test(
-            model,
-            train_loader,
-            test_loader,
-            optimizer=optimizer,
-            epochs=hparams['epochs'],
-            log_interval=hparams['log_interval'],
-            save_name=save_name + f'_{split}',
-            scheduler=get_scheduler(optimizer, hparams['warmup_steps']),
-            device=hparams['device'],
-            entropy=0
-        )
+print('Using tokenizer:', save_name)
+
+for split in range(N_FOLDS):
+    
+    print('Split:', split)
+    
+    test_data = []
+    with open(f'brown_data/brown_{split}.txt', 'r', encoding='utf-8') as f:
+        test_data = f.readlines()
+    
+    train_data = []
+    for i in range(N_FOLDS):
+        if i == split:
+            continue
+        train_data.extend(open(f'brown_data/brown_{i}.txt', 'r', encoding='utf-8').readlines())
+    
+    train_loader = DataLoader(
+        train_data,
+        batch_size=hparams['batch_size'],
+        shuffle=True,
+        collate_fn=collate_fn
+    )
+
+    test_loader = DataLoader(
+        test_data,
+        batch_size=hparams['batch_size'],
+        shuffle=False,
+        collate_fn=collate_fn
+    )
+    
+    optimizer = get_optimizer(model, hparams)
+    
+    model = get_model(**hparams)
+    
+    train_and_test(
+        model,
+        train_loader,
+        test_loader,
+        optimizer=optimizer,
+        epochs=hparams['epochs'],
+        log_interval=hparams['log_interval'],
+        save_name=save_name + f'_{split}',
+        scheduler=get_scheduler(optimizer, hparams['warmup_steps']),
+        device=hparams['device'],
+        entropy=0
+    )
