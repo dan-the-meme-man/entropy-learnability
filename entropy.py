fixed weird problem with masking

entropy.py
@@ -1,7 +1,12 @@
 import torch
 device = 'cpu'#torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-from generate_sequences import generate_bigram_sequences_using_table, generate_unigram_sequences_using_table
+from generate_sequences import (
+    generate_bigram_sequences_using_table,
+    generate_unigram_sequences_using_table
+)
+
+TOL = 5e-6
 
 def calculate_entropy_unigram(unigram_table: torch.Tensor) -> float:
     """
@@ -219,7 +224,7 @@ def sample_unigram_seqs_to_convergence(
             norm = torch.norm(p_next - p)
             print(f"Iteration {i}, Norm: {norm.item():.6f}")
             print(p)
-            if norm < 1e-6: # TODO: set more reasonable threshold - maybe 2e-5
+            if norm < TOL: # TODO: set more reasonable threshold - maybe 2e-5
                 break
         p = p_next
 
@@ -295,7 +300,7 @@ def sample_bigram_seqs_to_convergence(
             # difference between current next estimate and current estimate
             norm = torch.norm(p_next - p)
             print(f"Iteration {i}, Norm: {norm.item():.6f}")
-            if norm < 1e-6: # TODO: set more reasonable threshold - maybe 2e-5
+            if norm < TOL: # TODO: set more reasonable threshold - maybe 2e-5
                 break
         p = p_next
 

lstm.py
@@ -44,11 +44,13 @@ class LSTMLMHeadModel(nn.Module):
         self.n_layer = kwargs['n_layer']
         self.embd_pdrop = kwargs['embd_pdrop']
         self.lstm_pdrop = kwargs['attn_pdrop']
+        self.pad_token_id = kwargs['pad_token_id']
+        self.max_length = kwargs['sequence_length']
         
         self.embeddings = nn.Embedding(
             self.vocab_size,
             self.n_embd,
-            padding_idx=kwargs['pad_token_id']
+            padding_idx=self.pad_token_id
         )
         self.embeddings_dropout = nn.Dropout(self.embd_pdrop)
         self.lstm = nn.LSTM(
@@ -85,7 +87,7 @@ class LSTMLMHeadModel(nn.Module):
         if attention_mask is not None:
             lengths = attention_mask.sum(dim=1).cpu()
         else:
-            lengths = (input_ids != self.embeddings.padding_idx).sum(dim=1).cpu()
+            lengths = (input_ids != self.pad_token_id).sum(dim=1).cpu()
 
         # Embed the input tensor.
         x = self.embeddings(input_ids)
@@ -103,7 +105,12 @@ class LSTMLMHeadModel(nn.Module):
         x, (h_n, c_n) = self.lstm(x)
 
         # Unpack sequence
-        x, _ = nn.utils.rnn.pad_packed_sequence(x, batch_first=True)
+        x, _ = nn.utils.rnn.pad_packed_sequence(
+            x,
+            batch_first=True,
+            padding_value=self.pad_token_id,
+            total_length=self.max_length
+        )
 
         # Get the logits.
         x = self.output(x)

main.py
@@ -56,9 +56,12 @@ def main() -> None:
     torch.manual_seed(args.seed)
     np.random.seed(args.seed)
     random.seed(args.seed)
+    
+    debug = False
+    print('debug:', debug)
 
     hparams = {
-        'device': 'cuda' if torch.cuda.is_available() else 'cpu',
+        'device': 'cpu' if debug else 'cuda',
         'vocab_size': int(args.vocab_size),
         'n_positions': 64,
         'n_embd': 64, # 64, 256
@@ -73,15 +76,15 @@ def main() -> None:
         'pad_token_id': 2,
         'batch_size': 4,
         'sequence_length': 64,
-        'epochs': 4,
+        'epochs': 10 if not debug else 1,
         'learning_rate': 0.001,
         'warmup_steps': 100,
         'weight_decay': 0.01,
         'adam_epsilon': 1e-8,
         'max_grad_norm': 1.0,
         'dist': args.distribution,
-        'num_train_samples': 8000,
-        'num_test_samples': 2000,
+        'num_train_samples': 8000 if not debug else 100,
+        'num_test_samples': 2000 if not debug else 100,
         'log_interval': 10,
         'manual_option': args.manual_option
     }
@@ -152,17 +155,18 @@ def main() -> None:
         print(f'results/{save_name} already exists. Skipping training.')
         return
     
-    entropy = entropy_calc(probs)
-    print('Theoretical entropy:', entropy)
-    transient_entropy = transient_entropy_calc(
-        probs,
-        hparams['sequence_length'],
-        hparams['batch_size'],
-        hparams['bos_token_id'],
-        hparams['eos_token_id'],
-        hparams['pad_token_id']
-    )
-    print('Transient entropy:', transient_entropy)
+    if not debug:
+        entropy = entropy_calc(probs)
+        print('Theoretical entropy:', entropy)
+        transient_entropy = transient_entropy_calc(
+            probs,
+            hparams['sequence_length'],
+            hparams['batch_size'],
+            hparams['bos_token_id'],
+            hparams['eos_token_id'],
+            hparams['pad_token_id']
+        )
+        print('Transient entropy:', transient_entropy)
 
     print('Loading training data...')
     l_train = []
@@ -192,6 +196,7 @@ def main() -> None:
         model = get_lstm(**hparams)
     else:
         model = get_model(**hparams)
+        
     print('param count:', sum(p.numel() for p in model.parameters() if p.requires_grad))
     optimizer = get_optimizer(model, hparams)
     
@@ -205,8 +210,8 @@ def main() -> None:
         save_name=save_name,
         scheduler=get_scheduler(optimizer, hparams['warmup_steps']),
         device=hparams['device'],
-        entropy=entropy,
-        transient_entropy=transient_entropy,
+        entropy=entropy if not debug else None,
+        transient_entropy=transient_entropy if not debug else None,
         table=probs,
         pad_token_id=hparams['pad_token_id']
     )

train_model.py
@@ -39,6 +39,8 @@ def perplexity(
     
     total_loss = 0
     
+    loss_fn = torch.nn.CrossEntropyLoss(ignore_index=pad_token_id)
+    
     with torch.no_grad():
         for inputs in test_loader:
             
@@ -55,8 +57,16 @@ def perplexity(
                 'attention_mask': attention_mask.to(device)
             }
             
-            outputs = model(**inputs, labels=inputs['input_ids'])
-            loss = outputs.loss
+            outputs = model(**inputs, labels=None)#labels=inputs['input_ids'])
+            
+            labels = inputs['input_ids']
+            x = outputs.logits
+            shift_logits = x[..., :-1, :].contiguous()
+            shift_labels = labels[..., 1:].contiguous()
+            loss = loss_fn(
+                shift_logits.view(-1, shift_logits.size(-1)),
+                shift_labels.view(-1)
+            )
             
             total_loss += loss.item()
             
@@ -95,6 +105,7 @@ def train_and_test(
             scheduler: `_LRScheduler` - Learning rate scheduler.
             device: `str` - Device to train on.
     """
+    
     if os.path.exists(os.path.join('results', save_name + '.json')):
         print(f'results/{save_name} already exists. Skipping training.')
         return
@@ -103,6 +114,8 @@ def train_and_test(
     
     train_losses = []
     perplexities = []
+    
+    loss_fn = torch.nn.CrossEntropyLoss(ignore_index=pad_token_id)
     
     for epoch in range(epochs):
         print(f'Epoch {epoch + 1}')
@@ -125,20 +138,23 @@ def train_and_test(
                 input_ids = inputs.squeeze(0).to(device)
                 attention_mask = torch.ones_like(input_ids).to(device)
                 attention_mask[input_ids == pad_token_id] = 0
-                
-            if isinstance(model, GPT2LMHeadModel):
-                # GPT2 expects -100 for padding tokens
-                input_ids[input_ids == pad_token_id] = -100
-                
-            # LSTM expects custom pad_token_id, so no need to change it
             
             inputs = {
                 'input_ids': input_ids.to(device),
                 'attention_mask': attention_mask.to(device)
             }
             
-            outputs = model(**inputs, labels=inputs['input_ids'])
-            loss = outputs.loss
+            outputs = model(**inputs, labels=None)#labels=inputs['input_ids'])
+            
+            labels = inputs['input_ids']
+            x = outputs.logits
+            shift_logits = x[..., :-1, :].contiguous()
+            shift_labels = labels[..., 1:].contiguous()
+            
+            loss = loss_fn(
+                shift_logits.view(-1, shift_logits.size(-1)),
+                shift_labels.view(-1)
+            )
             
             loss.backward()
             optimizer.step()
@@ -158,7 +174,13 @@ def train_and_test(
                 msg += f'Avg Time: {avg_time:.3f}'
                 print(msg, flush=True)
                 
-        perplexities.append(perplexity(model, test_loader, device, text_data, pad_token_id))
+        perplexities.append(perplexity(
+            model,
+            test_loader,
+            device,
+            pad_token_id,
+            text_data
+        ))
 
     os.makedirs('models', exist_ok=True)
     os.makedirs('results', exist_ok=True)